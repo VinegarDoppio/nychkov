75cc3701c71623f5a1c077264256ed2839b9a064
18755 <18755@1k148s10.dune.edu>
2021-04-07 14:33:15 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+import entity.Studentyi;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import utils.NewHibernateUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        String sq1 = "FROM Studentyi s";
+        Query query = session.createQuery(sq1);
+        
+        List<Studentyi> rows = query.list();
+        
+        for(Studentyi row : rows) {
+            System.out.println(row.getGruppyi() + " " + row.getOtchestvo() + " " + row.getImya());
+        }
+        session.getTransaction().commit();
+        }
+}
